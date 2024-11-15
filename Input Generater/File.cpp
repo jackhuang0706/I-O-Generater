55915113126dc17015a53c64674aca78b1e3d584
fijjj <123882707+jackhuang0706@users.noreply.github.com>
2024-11-15 18:29:10 +0800

    Update File.cpp

--- Input Generater/File.cpp
+++ Input Generater/File.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
     int fileCount = 5;       // 設定要生成的檔案數量
-    int maxValue = 100;      // A 和 B 的最大值
 
-    srand(time(NULL));  // 初始化隨機種子
+    random_device rd;  // initialize
+    mt19937 gen(rd());
+    uniform_int_distribution<> value(1,1000); // range
 
     for (int fileNum = 1; fileNum <= fileCount; ++fileNum) {
-        string fileName = "資料夾路徑" + to_string(fileNum) + ".in";
+        string fileName = "資料夾路徑" + to_string(fileNum) + ".in"; //filename
         ofstream outFile(fileName);
 
         if (outFile.is_open()) {
             int a,b;
-            a=rand()%maxValue;
-            b=rand()%maxValue;
-            outFile << a << " " << b << endl;
+            a=value(gen),b=value(gen); //generate
+            outFile << a << " " << b << endl; //output
             outFile.close();
             cout << "檔案 " << fileName << " 已生成成功！" << endl;
         } else {
